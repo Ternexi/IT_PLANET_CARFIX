d9b0b77a85bc3813dec2fcd5e05c228df29593ce
Ternexi <shishkindmitrij333@gmail.com>
2025-10-05 19:13:22 +0300

    android studio

--- app/src/main/java/com/example/carfixapplication/MainActivity.kt
+++ app/src/main/java/com/example/carfixapplication/MainActivity.kt
@@ -1,6 +1,10 @@
 package com.example.carfixapplication
 
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ListView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
